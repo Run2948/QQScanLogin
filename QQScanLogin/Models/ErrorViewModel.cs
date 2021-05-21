b895c18c7e40f4e20c0b7b00c3bffbb17abc4d4b
zhujinrun <15997352948@163.com>
2021-05-21 16:38:14 +0800

    添加项目文件。

---
+++ QQScanLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QQScanLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
